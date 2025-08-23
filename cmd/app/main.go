7dfdfe4a5bf809d2c088326ae5d356d56160e01e
Moroz Roman <morozdef24@gmail.com>
2025-08-24 00:42:31 +0300

    update main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log/slog"
+	"os"
 
+	"l0-wb-tech/internal/database"
 	"l0-wb-tech/internal/logger"
 )
 
 func main() {
 	log := logger.New()
-	log.Info("Запуск сервиса...")
-	log.Debug("Режим отладки включен")
-	fmt.Println("Start project")
+	log.Info("Сервис запускается...")
+
+	const connStr string = "postgres://myuser:mypassword@localhost:5432/orders_db?sslmode=disable"
+
+	db, err := database.New(connStr, log)
+
+	if err != nil {
+		log.Error("Не удалось подключиться к базе данных", slog.Any("error", err))
+		os.Exit(1)
+	}
+
+	if err := db.Migrate(context.Background()); err != nil {
+		log.Error("Не удалось выполнить миграцию", slog.Any("error", err))
+		os.Exit(1)
+	}
+
 	log.Info("Сервис успешно запущен и готов к работе")
+
+	select {}
 }
