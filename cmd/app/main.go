c7b181702b6eec0b2cbc6b08950dea18e70ee5c1
Moroz Roman <morozdef24@gmail.com>
2025-08-24 23:32:59 +0300

    update main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"l0-wb-tech/internal/cache"
 	"l0-wb-tech/internal/database"
+	"l0-wb-tech/internal/handlers"
 	"l0-wb-tech/internal/logger"
+	"l0-wb-tech/internal/server"
 )
 
 func main() {
@@ -27,7 +35,46 @@ func main() {
 		os.Exit(1)
 	}
 
-	log.Info("Сервис успешно запущен и готов к работе")
+	orderCache := cache.New()
 
-	select {}
+	log.Info("Восстановление кэша из базы данных...")
+
+	orders, err := db.GetAllOrders(context.Background())
+
+	if err != nil {
+		log.Error("Не удалось загрузить заказы из БД для восстановления кэша", slog.Any("error", err))
+		os.Exit(1)
+	}
+
+	orderCache.Load(orders)
+
+	log.Info("Кэш успешно восстановлен", slog.Int("загружено_заказов", len(orders)))
+
+	httpHandler := handlers.New(orderCache, log)
+	httpServer := server.New("8081", httpHandler, log)
+
+	go func() {
+		if err := httpServer.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Error("HTTP-сервер аварийно остановлен", slog.Any("error", err))
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info("Сервис останавливается...")
+
+	const num_seconds int8 = 5
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Duration(num_seconds)*time.Second)
+
+	defer cancel()
+
+	if err := httpServer.Stop(shutdownCtx); err != nil {
+		log.Error("Ошибка при остановке HTTP-сервера", slog.Any("error", err))
+	}
+
+	log.Info("Сервис успешно остановлен")
 }
